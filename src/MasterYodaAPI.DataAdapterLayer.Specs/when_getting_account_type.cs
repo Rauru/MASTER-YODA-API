b1f5474184bfcb1eef978b4ad385fd15f3aad7ae
Camilo Rivera <camilo.ripi@gmail.com>
2016-02-01 20:51:34 -0600

    model carrera created

---
+++ src/MasterYodaAPI.DataAdapterLayer.Specs/when_getting_account_type.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+
+namespace MasterYodaAPI.DataAdapterLayer.Specs
+{
+    class when_getting_account_type
+    {
+
+
+        //Establish context = () =>
+        //{
+        //    Subject = new SecurityService(foo, bar);
+        //};
+
+        //Because of = () => Token = Subject.Authenticate("username", "password");
+
+        //It should_indicate_the_users_role = () => Token.Role.ShouldEqual(Roles.Admin);
+        //It should_have_a_unique_session_id = () => Token.SessionId.ShouldNotBeNull();
+
+        //static SecurityService Subject;
+        //static UserToken Token;
+
+    }
+}
---
+++ src/MasterYodaAPI.DataAdapterLayer/Models/Carrera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterYodaAPI.DataAdapterLayer.Models
+{
+    public class Carrera
+    {
+        public int carreraID { get; set; }
+        public string carrera { get; set; }
+    }
+}
